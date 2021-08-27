07c3f3747d30d077ebdefc3657672ebdfc104fb1
Josh Daniel <joshpersonal8@gmail.com>
2021-08-27 16:02:21 +0800

    ✨Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className="h-screen bg-gray-900 text-gray-200 flex items-center justify-center">
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
